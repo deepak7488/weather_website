b5ac90ef7260a54c4fc82ee3d5eba3d5e7bd1194
Deepak <deepak.kumar@iiitg.ac.in>
2021-09-18 18:55:27 +0530

    Remove unnecessary console.log call

--- public1/js/app.js
+++ public1/js/app.js
@@ -1,4 +1,3 @@
-// console.log("Clinet side java script File is loaded");
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
